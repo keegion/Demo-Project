21403bf24afe007a21e75f824a165b5a42b6083b
mihkel tanel <mihkel.tanel@edu.amiedu.fi>
2019-01-23 13:20:57 +0200

    Done so far: working chat,chat page, login page

---
+++ DemoProject/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace DemoProject.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string time, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, time, message);
+        }
+    }
+}
---
+++ DemoProject/wwwroot/js/chat.js
@@ -0,0 +1,57 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+//recieve message
+connection.on("ReceiveMessage", function (user, time, message) {
+   
+    var msg = '<b>'+ user +'</b> '+time+' </br>'+ message+ '<hr>';
+    $('#messagesList').append(msg);
+    //scroll down automatically
+    $("#messagesList").stop().animate({ scrollTop: $("#messagesList")[0].scrollHeight }, 1000);
+   
+});
+
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+//Send Button
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    SendMessage();
+});
+
+//Enter button
+
+document.getElementById('messageInput').onkeydown = function (e) {
+    if (e.keyCode == 13) {
+        SendMessage();
+    }
+};
+
+//Send message
+function SendMessage() {
+
+    var message = document.getElementById("messageInput").value;
+    if (message) {
+        var user = "TestUser";
+        var time = FormatDate(new Date());
+        connection.invoke("SendMessage", user, time, message).catch(function (err) {
+            return console.error(err.toString());
+
+        });
+        clearInput();
+    }
+}
+
+//date time format
+function FormatDate(now){
+    var formatedDate = dateFormat(now, "dd/mm HH:MM")
+    return formatedDate;
+}
+// clear message input
+function clearInput() {
+    $(".messageBar :input").each(function () {
+        $(this).val(''); //hide form values
+    });
+}
