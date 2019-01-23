e0d89c7e6373714f50f00fb2f09fcbb2cddd8976
mihkel tanel <mihkel.tanel@edu.amiedu.fi>
2019-01-23 16:44:51 +0200

    Database set up

---
+++ DemoProject/Models/Accounts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoProject.Models
+{
+    public partial class Accounts
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DemoProject/Models/DemoProjectContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DemoProject.Models
+{
+    public partial class DemoProjectContext : DbContext
+    {
+        public DemoProjectContext()
+        {
+        }
+
+        public DemoProjectContext(DbContextOptions<DemoProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Accounts> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=DemoProject;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Accounts>(entity =>
+            {
+                entity.HasKey(e => e.Username);
+
+                entity.Property(e => e.Username)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
